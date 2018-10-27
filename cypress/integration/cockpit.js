140fbcbbd56eb844e71ae6bcdc83736b14a06b1c
Martin Pitt <martin@piware.de>
2018-10-27 23:22:33 +0200

    Skip login page for frame tests
    Use Basic Auth + X-Authorize header to directly go to the tested frame,
    skipping the login page. Only do the graphical login for testing the
    login page itself.
    
    https://docs.cypress.io/guides/references/best-practices.html#Organizing-Tests-Logging-In-Controlling-State

--- cypress/integration/cockpit.js
+++ cypress/integration/cockpit.js
@@ -1,35 +1,44 @@
-function login() {
-    cy.visit("/system");
-    cy.get('#authorized-input').
-        click().
-        should('be.checked');
-    cy.get('#login-user-input').type('admin');
-    cy.get('#login-password-input').type('foobar{enter}');
-    cy.get('#host-nav').should('be.visible');
-}
+// Use this for skipping the login page, i. e. all tests which do not test the login page itself
+const visit_opts = { auth: { username: 'admin', password: 'foobar' } };
 
 describe('cockpit demo', () => {
     beforeEach('start VM', function () {
         cy.task('startVM', 'fedora-29').then(url => Cypress.config('baseUrl', url));
+
+        // Programmatically enable the "Reuse my password for privileged tasks" option
+        cy.server({
+            onAnyRequest: function (route, proxy) {
+                proxy.xhr.setRequestHeader('X-Authorize',  'password');
+            }
+        });
     });
 
     afterEach('stop VM', function() {
         cy.task('stopVM');
     });
 
-    it('"Other options" expansion', function () {
+    it('Login page', function () {
         cy.visit('/');
+
+        // "Other options"
         cy.get('#server-field').should('not.be.visible');
         cy.get('#show-other-login-options').should('be.visible').click();
         cy.get('#server-field').should('be.visible');
         cy.get('#show-other-login-options').click();
         cy.get('#server-field').should('not.be.visible');
+
+        // log in
+        cy.get('#authorized-input').
+            click().
+            should('be.checked');
+        cy.get('#login-user-input').type('admin');
+        cy.get('#login-password-input').type('foobar{enter}');
+        cy.get('#host-nav').should('be.visible');
     });
 
     it('Change hostname', function () {
-        login();
         // HACK: cypress doesn't handle frames, so go to specific frame
-        cy.visit("/cockpit/@localhost/system/index.html");
+        cy.visit('/cockpit/@localhost/system/index.html', visit_opts)
         cy.get('#system_information_hostname_button').
             should('contain', 'localhost.localdomain').
             click();
